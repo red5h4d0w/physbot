008f1cc5d9c0622d3046d389685fb876174387ff
François Hardy <snaptraks@gmail.com>
2020-09-13 12:25:38 -0400

    Implement sqlite DB to the bot, and save conseils in it

--- cogs/information.py
+++ cogs/information.py
@@ -1,46 +1,46 @@
 import aiohttp
-import json
-from discord.ext import commands
+import aiosqlite
+from discord.ext import commands, tasks
 
 
 class Information(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._create_tables.start()
 
     @commands.group(invoke_without_command=True)
-    async def conseil(self, ctx, *mots):
+    async def conseil(self, ctx, *, mots: str):
         '''donne un conseil sur ce que vous demandez si disponible'''
-        if not len(mots) == 1:
-            mot = "".join(mots)
+
+        row = await self._get_conseil(mots)
+        if row is None:
+            await ctx.send("Il n'y a pas de conseil sous cette clé.")
 
         else:
-            mot = mots[0]
-        try:
-            # version pour  database sqlite3
-            # async with aiosqlite.connect("infos.db") as conn:
-            #     async with conn.cursor() as c:
-            #         conseil = await c.execute("SELECT * FROM conseil WHERE nom=?",mot)
-            # await ctx.send(conseil)
-            with open("conseil.json") as json_file:
-                data = json.load(json_file)
-                info = data[mot]
-            if info:
-                await ctx.send(info)
-        except:
-            await ctx.send("désolé, je ne peux pas t'aider :(")
+            await ctx.send(row['conseil'])
 
     @conseil.command(name='create')
-    async def conseil_create(self, ctx, motclé: str, *conseil):
-        # version pour database sqlite3
-        # async with aiosqlite.connect("infos.db") as conn:
-        #     async with conn.cursor() as c:
-        #         await c.execute("INSERT INTO conseil (nom) VALUES (?)", "".join(conseil))
-        with open("conseil.json", "r+") as json_file:
-            data = json.load(json_file)
-            data[motclé] = " ".join(conseil)
-            json_file.seek(0)
-            json.dump(data, json_file, indent=4)
-            json_file.truncate()
+    async def conseil_create(self, ctx, motclé: str, *, conseil: str):
+        """Subcommande pour créer un nouveau conseil."""
+
+        await self._save_conseil(ctx, motclé, conseil)
+
+        await ctx.send(
+            f"OK! J'ai enregistré le conseil sous la clé `{motclé}`.")
+
+    @conseil_create.error
+    async def conseil_create_error(self, ctx, _error):
+        """Error handler for the conseil_create command."""
+
+        error = getattr(_error, 'original', _error)
+
+        if isinstance(error, aiosqlite.IntegrityError):
+            await ctx.send("Il y a déjà un conseil avec cette clé.")
+
+        else:
+            raise _error
+
+    # TODO: conseil_delete, conseil_update commands
 
     @commands.command()
     async def wiki(self, ctx, *nom_article):
@@ -54,6 +54,60 @@ class Information(commands.Cog):
                     await ctx.send(url)
                 else:
                     await ctx.send("Désolé, je n'ai pas réussi à trouver l'article")
+
+    @tasks.loop(count=1)
+    async def _create_tables(self):
+        """Crée les tables nécessaires, si elles n'existent pas déjà."""
+
+        await self.bot.db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS information_conseil(
+                conseil    TEXT      NOT NULL,
+                created_at TIMESTAMP NOT NULL,
+                key        TEXT      NOT NULL UNIQUE,
+                user_id    INTEGER   NOT NULL
+            )
+            """
+        )
+
+        await self.bot.db.commit()
+
+    async def _get_conseil(self, key):
+        """Cherche le conseil avec la clé 'key' dans la base de données.
+        Peut retourner None si la clé n'existe pas.
+        """
+        async with self.bot.db.execute(
+                """
+                SELECT *
+                  FROM information_conseil
+                 WHERE key = :key
+                """,
+                {'key': key}
+        ) as c:
+            row = await c.fetchone()
+
+        return row
+
+    async def _save_conseil(self, ctx, key, conseil):
+        """Enregistre le conseil dans la base de données."""
+
+        await self.bot.db.execute(
+            """
+            INSERT INTO information_conseil
+            VALUES (:conseil,
+                    :created_at,
+                    :key,
+                    :user_id)
+            """,
+            {
+                'conseil': conseil,
+                'created_at': ctx.message.created_at,
+                'key': key,
+                'user_id': ctx.author.id,
+            }
+        )
+
+        await self.bot.db.commit()
 
 
 def setup(bot):
