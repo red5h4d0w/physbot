769c6e8381ef0324e6c0735cf8b1801c908d55f1
Snaptraks <snaptraks@gmail.com>
2020-11-10 22:18:51 -0500

    Use all intents for now

--- bot.py
+++ bot.py
@@ -37,14 +37,14 @@ if __name__ == '__main__':
     with open("token.txt") as f:
         TOKEN = f.read()
 
-    intents = discord.Intents(emojis=True, guilds=True, members=True, messages=True)
+    intents = discord.Intents.all()
 
     bot = PhysBot(
         description="Bot pour le serveur Discord de la PHYSUM!",
         command_prefix="p.",
         activity=discord.Game(name="aider la Physum"),
-        db_name='physbot.db',
         intents=intents
+        db_name='physbot.db',
     )
 
     startup_extensions = [
